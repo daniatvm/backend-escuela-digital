c599f57213fb2cb91b1b3be1337fd0ecd83cf35d
daniatvm <daniatvm@gmail.com>
2019-10-07 20:54:55 -0600

    creacion de controladores y rutas

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,16 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::resource('access_type', 'Access_TypeController');
+Route::resource('board_of_education', 'Board_of_EducationController');
+Route::resource('class_x_employee', 'Class_x_EmployeeController');
+Route::resource('employee', 'EmployeeController');
+Route::resource('feedback', 'Feedback_Controller');
+Route::resource('gallery', 'GalleryController');
+Route::resource('job', 'JobController');
+Route::resource('level', 'LevelController');
+Route::resource('new_type', 'New_TypeController');
+Route::resource('new', 'NewController');
+Route::resource('school', 'SchoolController');
+Route::resource('user', 'UserController');
